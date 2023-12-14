2326f1ccde9e4345db36b11bc91ab420b1d5466e
felipe <felipecleones999@gmail.com>
2023-12-14 20:43:23 -0300

    add

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent {
+  @Input()
+  photoCover:string ="https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png"
+  @Input()
+  cardTitle:string="a"
+  @Input()
+  cardDescription:string="a"
 
+  constructor(){}
 }
